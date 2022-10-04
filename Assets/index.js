607df8e842f13b2a8fb66546f7f1d85e4ad1a75f
sam song <samuelsong18@gmail.com>
2022-10-03 20:33:16 -0400

    node app is working but need to change code to ask questions correctly

--- Assets/index.js
+++ Assets/index.js
@@ -6,192 +6,178 @@ const PrincipalSoftwareEngineer = require("./utils/principalSoftwareEngineer");
 const SeniorSoftwareEngineer = require("./utils/seniorSoftwareEngineer");
 const SoftwareEngineer = require("./utils/softwareEngineer");
 const Intern = require("./utils/intern");
-const generateHtml = require("./utils/generateMarkup");
+const mergeAllRoles = require("./utils/generateMarkup");
+const outputDirectory = path.resolve(__dirname, "Team");
+const outputPath = path.join(outputDirectory, "index.html");
 const fullTeam = [];
 
-const questions = [
-  // MANAGER INPUTS
-  {
-    type: "input",
-    name: "manager",
-    message: "What is the managers name?",
-    validate: (managerInput) => {
-      if (managerInput) {
-        return true;
-      } else {
-        console.log("There needs to be a manager!");
-        return false;
-      }
-    },
-  },
-  {
-    type: "input",
-    name: "managerId",
-    message: "What is your Employee ID?",
-    validate: (idInput) => {
-      if (idInput) {
-        return true;
-      } else {
-        console.log("You need an employee ID!");
-        return false;
-      }
-    },
-  },
-  {
-    type: "input",
-    name: "managerEmail",
-    message: "What is your Email address?",
-    validate: (emailInput) => {
-      if (emailInput) {
-        return true;
-      } else {
-        console.log("You need an email for updates!");
-        return false;
-      }
-    },
-  },
-  {
-    type: "input",
-    name: "number",
-    message: "What is the office number?",
-    validate: (numberInput) => {
-      if (numberInput) {
-        return true;
-      } else {
-        console.log("Office number. NOW!");
-        return false;
-      }
-    },
-  },
-  // LIST INPUT
-  {
-    type: "list",
-    name: "menu",
-    message: "Would you like to add a team member?",
-    choices: ["Engineer", "Intern", "Finish Building My Team"],
-    validate: (menuInput) => {
-      if (menuInput) {
-        return true;
-      } else {
-        console.log("YOU NEED TO PICK ONE!");
-        return false;
-      }
-    },
-  },
-  // ENGINEER INPUTS!!
-  {
-    type: "input",
-    name: "engineerName",
-    message: "What is the engineers name?",
-    validate: (engineerInput) => {
-      if (engineerInput) {
-        return true;
-      } else {
-        console.log("We both know he has a name!");
-        return false;
-      }
-    },
-  },
-  {
-    type: "input",
-    name: "engineerId",
-    message: "What is the engineers Employee ID?",
-    validate: (engineerIdInput) => {
-      if (engineerIdInput) {
-        return true;
-      } else {
-        console.log("You need an employee ID!");
-        return false;
-      }
-    },
-  },
-  {
-    type: "input",
-    name: "engineersEmail",
-    message: "What is the engineers Email address?",
-    validate: (engineerEmailInput) => {
-      if (engineerEmailInput) {
-        return true;
-      } else {
-        console.log("You need an email for updates!");
-        return false;
-      }
-    },
-  },
-  {
-    type: "input",
-    name: "github",
-    message: "What is the engineers GitHub username?",
-  },
-  // INTERN INPUTS!!!
-  {
-    type: "input",
-    name: "internName",
-    message: "What is the interns name?",
-    validate: (internInput) => {
-      if (internInput) {
-        return true;
-      } else {
-        console.log("We both know he has a name!");
-        return false;
-      }
-    },
-  },
-  {
-    type: "input",
-    name: "internId",
-    message: "What is the interns Employee ID?",
-    validate: (internIdInput) => {
-      if (internIdInput) {
-        return true;
-      } else {
-        console.log("You need an employee ID!");
-        return false;
-      }
-    },
-  },
-  {
-    type: "input",
-    name: "internsEmail",
-    message: "What is the interns Email address?",
-    validate: (internEmailInput) => {
-      if (internEmailInput) {
-        return true;
-      } else {
-        console.log("You need an email for updates!");
-        return false;
-      }
-    },
-  },
-  {
-    type: "input",
-    name: "school",
-    message: "What school does the intern attend?",
-    validate: (ineternSchoolInput) => {
-      if (ineternSchoolInput) {
-        return true;
-      } else {
-        console.log(
-          "The intern needs to be in school to qualify for the intern role."
-        );
-        return false;
-      }
-    },
-  },
-];
+const createManager = () => {
+  console.log(
+    "Hello \n Welcome to the Team Builder! \n  Answer questions regaurding employee type and information! \n After you are finished, a new web application will be made!"
+  );
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the Manager's name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is the Manager's ID#?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the Manager's email?",
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "What is the Office Number?",
+      },
+    ])
+    .then((managerInfo) => {
+      managerInfo.role = "Manager";
+      const { name, id, email, officeNumber } = managerInfo;
+      const newManager = new Manager(name, id, email, officeNumber);
+      fullTeam.push(newManager);
+    });
+};
 
-function writetoFile(fileName, data) {
-  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
-}
+const addEmployees = () => {
+  return inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "role",
+        message: "What is the employee's role?",
+        choices: [
+          "PrincipalSoftwareEngineer",
+          "SeniorSoftwareEngineer",
+          "SoftwareEngineer",
+          "Intern",
+        ],
+      },
+      {
+        type: "input",
+        name: "name",
+        message: "What is the employees name?",
+        validate: (nameInput) => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log("Enter the employees name!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is the employees ID#?",
+        validate: (idInput) => {
+          if (idInput) {
+            return true;
+          } else {
+            console.log("Please enter ID#!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the employees email?",
+        validate: (emailInput) => {
+          if (emailInput) {
+            return true;
+          } else {
+            console.log("Enter the employees email!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "gitHub",
+        message: "What is the SoftwareEngineer's GitHub?",
+        validate: (gitHubInput) => {
+          if (gitHubInput) {
+            return true;
+          } else {
+            console.log(
+              "Please enters the SoftwareEngineer's GitHub so you and other people can see his amazing work!"
+            );
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "Where does the intern attend school?",
+        validate: (schoolInput) => {
+          if (schoolInput) {
+            return true;
+          } else {
+            console.log(
+              "Intern must attend school to qualify for intern position!"
+            );
+            return false;
+          }
+        },
+      },
+      {
+        type: "confirm",
+        name: "add",
+        message: "Would you like to add another team member?",
+        default: false,
+      },
+    ])
+    .then((employeeData) => {
+      let { name, id, email, role, gitHub, school, add } = employeeData;
+      let employee;
+      if (role === "PrincipalSoftwareEngineer") {
+        employee = new PrincipalSoftwareEngineer(name, id, email, gitHub);
+      } else if (role === "SeniorSoftwareEnginner") {
+        employee = new SeniorSoftwareEngineer(name, id, email, gitHub);
+      } else if (role === "SoftwareEngineer") {
+        employee = new SoftwareEngineer(name, id, email, gitHub);
+      } else if (role === "Intern") {
+        employee = new Intern(name, id, email, school);
+      }
+      fullTeam.push(employee);
+      if (add) {
+        return addEmployees(fullTeam);
+      } else {
+        return fullTeam;
+      }
+    });
+};
 
-function init() {
-  inquirer.prompt(questions).then((inquirerAnswers) => {
-    console.log("Generating roster... Please wait...");
-    writetoFile("./index.html", internMarkdown({ ...inquirerAnswers }));
-    writetoFile("./index.html", engineerMarkdown({ ...inquirerAnswers }));
-    writetoFile("./index.html", managerMarkdown({ ...inquirerAnswers }));
-    writetoFile("./index.html", generateMarkdown({ ...inquirerAnswers }));
+const writeFile = (data) => {
+  fs.writeFile(outputPath, data, (err) => {
+    if (err) {
+      console.log(err);
+      return;
+    } else {
+      console.log(
+        "Team Made Succesfully! \n You can find it in the Team Folder!"
+      );
+    }
   });
-}
+};
 
-init();
+createManager()
+  .then(addEmployees)
+  .then((fullTeam) => {
+    return mergeAllRoles(fullTeam);
+  })
+  .then((html) => {
+    return writeFile(html);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
--- Assets/utils/generateMarkup.js
+++ Assets/utils/generateMarkup.js
@@ -1,19 +1,19 @@
-const manager = (manager) => {
+const Manager = (manager) => {
   return ` <div class="card m-2" style="width: 18rem">
     <div class="card-body bg-primary">
-    //MANAGER NAME
+    <!-- name -->
       <h5 class="card-title">${manager.name}</h5>
       <h6 class="card-title">Manager</h6>
     </div>
     <ul class="list-group list-group-flush">
-    //MANAGER ID
+    <!--ID NUMBER-->
       <li class="list-group-item">ID: ${manager.id}</li>
       <li class="list-group-item">
-      //MANAGER EMAIL
+      <!-- email-->
         Email: <a href="mailto:${manager.email}" class="card-link">${manager.email}</a>
       </li>
       <li class="list-group-item">
-      //OFFICE NUMBER
+      <!--Extra-->
         Office Number: ${manager.officeNumber}
       </li>
     </ul>
@@ -22,20 +22,20 @@ const manager = (manager) => {
 const PrincipalSoftwareEngineer = (principalSoftwareEngineer) => {
   return `<div class="card m-2" style="width: 18rem">
     <div class="card-body bg-success">
-    //ENGINEER NAME
+    <!-- name -->
       <h5 class="card-title">${principalSoftwareEngineer.name}</h5>
       <h6 class="card-title">Engineer</h6>
     </div>
     <ul class="list-group list-group-flush">
-    //ENGINEER ID
+    <!--ID NUMBER-->
       <li class="list-group-item">${principalSoftwareEngineer.id}</li>
       <li class="list-group-item">
-      // EGNINEER EMAIL
+      <!-- email-->
         Email: <a href="mailto:${principalSoftwareEngineer.email}" class="card-link">${principalSoftwareEngineer.email}</a>
       </li>
       <li class="list-group-item">
-      //EGNINEER GITHUB
-        GitHub: <a href="github.com/${principalSoftwareEngineer.github}" class="card-link">${principalSoftwareEngineer.github}</a>
+      <!--Extra-->
+        GitHub: <a href="github.com/${principalSoftwareEngineer.gitHub}" class="card-link">${principalSoftwareEngineer.gitHub}</a>
       </li>
     </ul>
   </div>`;
@@ -43,19 +43,19 @@ const PrincipalSoftwareEngineer = (principalSoftwareEngineer) => {
 const SeniorSoftwareEngineer = (seniorSoftwareEngineer) => {
   return `<div class="card m-2" style="width: 18rem">
     <div class="card-body bg-success">
-    //ENGINEER NAME
+    <!-- name -->
       <h5 class="card-title">${seniorSoftwareEngineer.name}</h5>
       <h6 class="card-title">Engineer</h6>
     </div>
     <ul class="list-group list-group-flush">
-    //ENGINEER ID
+    <!--ID NUMBER-->
       <li class="list-group-item">${seniorSoftwareEngineer.id}</li>
       <li class="list-group-item">
-      // EGNINEER EMAIL
+      <!-- email-->
         Email: <a href="mailto:${seniorSoftwareEngineer.email}" class="card-link">${seniorSoftwareEngineer.email}</a>
       </li>
       <li class="list-group-item">
-      //EGNINEER GITHUB
+      <!--Extra-->
         GitHub: <a href="github.com/${seniorSoftwareEngineer.gitHub}" class="card-link">${seniorSoftwareEngineer.gitHub}</a>
       </li>
     </ul>
@@ -64,39 +64,40 @@ const SeniorSoftwareEngineer = (seniorSoftwareEngineer) => {
 const SoftwareEngineer = (softwareEngineer) => {
   return `<div class="card m-2" style="width: 18rem">
     <div class="card-body bg-success">
-    //ENGINEER NAME
+    <!-- name -->
       <h5 class="card-title">${softwareEngineer.name}</h5>
       <h6 class="card-title">Engineer</h6>
     </div>
     <ul class="list-group list-group-flush">
-    //ENGINEER ID
+    <!-- ID NUMBER-->
       <li class="list-group-item">${softwareEngineer.id}</li>
       <li class="list-group-item">
-      // EGNINEER EMAIL
+      <!-- email-->
         Email: <a href="mailto:${softwareEngineer.email}" class="card-link">${softwareEngineer.email}</a>
       </li>
       <li class="list-group-item">
-      //EGNINEER GITHUB
+      <!--Extra-->
         GitHub: <a href="github.com/${softwareEngineer.gitHub}" class="card-link">${softwareEngineer.gitHub}</a>
       </li>
     </ul>
   </div>`;
 };
 
-const intern = (intern) => {
+const Intern = (intern) => {
   return `<div class="card m-2" style="width: 18rem">
   <div class="card-body bg-danger">
-  <!-- Interns name -->
+  <!-- name -->
     <h5 class="card-title">${intern.name}</h5>
     <h6 class="card-title">Intern</h6>
   </div>
   <ul class="list-group list-group-flush">
-  <!--Interns ID NUMBER-->
+  <!--ID NUMBER-->
     <li class="list-group-item">${intern.id}</li>
     <li class="list-group-item">
-    <!--Interns email-->
+    <!-- email-->
       Email: <a href="mailto:${intern.email}" class="card-link">${intern.email}</a>
     </li>
+    <!--Extra-->
     <li class="list-group-item">
       School: ${intern.school}
     </li>
@@ -110,23 +111,23 @@ const mergeAllRoles = (data) => {
     const employee = data[i];
     const role = employee.getRole();
     if (role === "Manager") {
-      const managerCard = manager(employee);
+      const managerCard = Manager(employee);
       cardArray.push(managerCard);
     }
     if (role === "PrincipalSoftwareEngineer") {
-      const principalSoftwareEngineerCard = PrincipalSoftwareEngineer();
+      const principalSoftwareEngineerCard = PrincipalSoftwareEngineer(employee);
       cardArray.push(principalSoftwareEngineerCard);
     }
-    if (role === "seniorSoftwareEnginner") {
-      const seniorSoftwareEngineerCard = SeniorSoftwareEngineer();
+    if (role === "SeniorSoftwareEnginner") {
+      const seniorSoftwareEngineerCard = SeniorSoftwareEngineer(employee);
       cardArray.push(seniorSoftwareEngineerCard);
     }
-    if (role === "softwareEnginner") {
-      const softwareEngineerCard = SoftwareEngineer();
+    if (role === "SoftwareEnginner") {
+      const softwareEngineerCard = SoftwareEngineer(employee);
       cardArray.push(softwareEngineerCard);
     }
-    if (role === "intern") {
-      const internCard = Intern();
+    if (role === "Intern") {
+      const internCard = Intern(employee);
       cardArray.push(internCard);
     }
   }
@@ -165,4 +166,4 @@ const generateHtml = (displayTeam) => {
     </html>`;
 };
 
-module.exports = generateHtml;
+module.exports = mergeAllRoles;
